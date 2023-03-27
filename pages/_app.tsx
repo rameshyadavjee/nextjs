04efa99bdfa04b25e8fe3502ac6cc96fccbe64ea
Ramesh Yadav <ramesh@aceinfoway.com>
2023-03-27 19:27:58 +0530

    Upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,15 @@
-import '@/styles/globals.css'
+import '@/styles/style.css'
+import '@/styles/vendor/animate/animate.min.css' 
+import '@/styles/vendor/bootstrap/css/bootstrap.min.css'
+import '@/styles/vendor/bootstrap-icons/bootstrap-icons.css'
+import '@/styles/vendor/boxicons/css/boxicons.min.css'
+import '@/styles/vendor/glightbox/css/glightbox.min.css'
+import '@/styles/vendor/swiper/swiper-bundle.min.css'
+
+
+
+ 
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
